9127f27d966598b061dc74c3c3c25034209f972b
Christian Guevara <chrisdb150@gmail.com>
2022-08-11 16:57:43 -0400

    deploying to devnet

--- programs/solana-movies/src/lib.rs
+++ programs/solana-movies/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7EZkj1Y7frBDwRH5g72xqaYhLBRRH6b9vRkgLHSJA9bq");
 
 #[program]
 pub mod solana_movies {
